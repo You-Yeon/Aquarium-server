f7e64ea09b94c611741cd8387a8380b3854bccd3
choiyuyeon <cyy0067@naver.com>
2020-01-08 03:00:22 +0900

    Server / Common init

---
+++ Aquarium_server/Server/framework.h
@@ -0,0 +1,20 @@
+﻿// header.h: 표준 시스템 포함 파일
+// 또는 프로젝트 특정 포함 파일이 들어 있는 포함 파일입니다.
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+//#include <tchar.h>
+
+#include <ProudNetServer.h>
+#include <memory>
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <string>
+
+using namespace Proud;
+using namespace std;
