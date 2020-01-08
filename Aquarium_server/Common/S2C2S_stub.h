d0e9570196f92ba3f29a35a6af03d8987c60d103
choiyuyeon <cyy0067@naver.com>
2020-01-08 20:06:45 +0900

    S/C C/S Network success

--- Aquarium_server/Common/S2C2S_stub.h
+++ Aquarium_server/Common/S2C2S_stub.h
@@ -13,3 +13,88 @@
 
 #include "S2C2S_common.h"
 
+     
+namespace S2C2S {
+
+
+	class Stub : public ::Proud::IRmiStub
+	{
+	public:
+               
+		virtual bool RequestLogin ( ::Proud::HostID, ::Proud::RmiContext& , const std::wstring & , const std::wstring & )		{ 
+			return false;
+		} 
+
+#define DECRMI_S2C2S_RequestLogin bool RequestLogin ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext , const std::wstring & id, const std::wstring & password) PN_OVERRIDE
+
+#define DEFRMI_S2C2S_RequestLogin(DerivedClass) bool DerivedClass::RequestLogin ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext , const std::wstring & id, const std::wstring & password)
+#define CALL_S2C2S_RequestLogin RequestLogin ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext , const std::wstring & id, const std::wstring & password)
+#define PARAM_S2C2S_RequestLogin ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext , const std::wstring & id, const std::wstring & password)
+               
+		virtual bool NotifyLoginSuccess ( ::Proud::HostID, ::Proud::RmiContext& )		{ 
+			return false;
+		} 
+
+#define DECRMI_S2C2S_NotifyLoginSuccess bool NotifyLoginSuccess ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext ) PN_OVERRIDE
+
+#define DEFRMI_S2C2S_NotifyLoginSuccess(DerivedClass) bool DerivedClass::NotifyLoginSuccess ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext )
+#define CALL_S2C2S_NotifyLoginSuccess NotifyLoginSuccess ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext )
+#define PARAM_S2C2S_NotifyLoginSuccess ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext )
+               
+		virtual bool NotifyLoginFailed ( ::Proud::HostID, ::Proud::RmiContext& , const std::wstring & )		{ 
+			return false;
+		} 
+
+#define DECRMI_S2C2S_NotifyLoginFailed bool NotifyLoginFailed ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext , const std::wstring & reason) PN_OVERRIDE
+
+#define DEFRMI_S2C2S_NotifyLoginFailed(DerivedClass) bool DerivedClass::NotifyLoginFailed ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext , const std::wstring & reason)
+#define CALL_S2C2S_NotifyLoginFailed NotifyLoginFailed ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext , const std::wstring & reason)
+#define PARAM_S2C2S_NotifyLoginFailed ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext , const std::wstring & reason)
+ 
+		virtual bool ProcessReceivedMessage(::Proud::CReceivedMessage &pa, void* hostTag) PN_OVERRIDE;
+		static const PNTCHAR* RmiName_RequestLogin;
+		static const PNTCHAR* RmiName_NotifyLoginSuccess;
+		static const PNTCHAR* RmiName_NotifyLoginFailed;
+		static const PNTCHAR* RmiName_First;
+		virtual ::Proud::RmiID* GetRmiIDList() PN_OVERRIDE { return g_RmiIDList; }
+		virtual int GetRmiIDListCount() PN_OVERRIDE { return g_RmiIDListCount; }
+	};
+
+#ifdef SUPPORTS_CPP11 
+	
+	class StubFunctional : public Stub 
+	{
+	public:
+               
+		std::function< bool ( ::Proud::HostID, ::Proud::RmiContext& , const std::wstring & , const std::wstring & ) > RequestLogin_Function;
+		virtual bool RequestLogin ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext , const std::wstring & id, const std::wstring & password) 
+		{ 
+			if (RequestLogin_Function==nullptr) 
+				return true; 
+			return RequestLogin_Function(remote,rmiContext, id, password); 
+		}
+
+               
+		std::function< bool ( ::Proud::HostID, ::Proud::RmiContext& ) > NotifyLoginSuccess_Function;
+		virtual bool NotifyLoginSuccess ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext ) 
+		{ 
+			if (NotifyLoginSuccess_Function==nullptr) 
+				return true; 
+			return NotifyLoginSuccess_Function(remote,rmiContext); 
+		}
+
+               
+		std::function< bool ( ::Proud::HostID, ::Proud::RmiContext& , const std::wstring & ) > NotifyLoginFailed_Function;
+		virtual bool NotifyLoginFailed ( ::Proud::HostID remote, ::Proud::RmiContext& rmiContext , const std::wstring & reason) 
+		{ 
+			if (NotifyLoginFailed_Function==nullptr) 
+				return true; 
+			return NotifyLoginFailed_Function(remote,rmiContext, reason); 
+		}
+
+	};
+#endif
+
+}
+
+
--- Aquarium_server/Server/Server.h
+++ Aquarium_server/Server/Server.h
@@ -1,10 +1,15 @@
 ﻿#pragma once
 
+#include "../Common/Common.h"
+#include "../Common/S2C2S_proxy.h"
+#include "../Common/S2C2S_stub.h"
+
 // 클라이언트 클래스
 class RemoteClient
 {
 public:
 	std::wstring m_userID; // 유저의 HostID 값 (*KEY)
+	int m_character; // 유저의 캐릭터 종류 정보
 	float m_x, m_y, m_z; // 유저의 위치 값
 	float m_vx, m_vy, m_vz; // 유저의 속력 값
 	float m_angle; // 유저의 회전 값
@@ -13,10 +18,21 @@ public:
 
 // 서버 클래스
 class Aquarium_server
+	:public S2C2S::Stub
 {
 public:
+
+	// RMI 보내기 위한 모듈
+	S2C2S::Proxy m_proxy;
+
 	// 클라이언트의 연결을 받기위한 객체
 	shared_ptr<CNetServer> m_netServer;
+
+	// 멀티스레드의 경쟁 상태를 예방하기 위해서 잠금
+	CriticalSection m_critSec;
+
+	// key : client HostID
+	unordered_map<int, shared_ptr<RemoteClient> > m_remoteClients;
 
 	Aquarium_server()
 	{
@@ -29,5 +45,8 @@ public:
 	void Run(); // 서버 런타임 함수
 
 private:
+
+	// 로그인 원격 함수
+	DECRMI_S2C2S_RequestLogin;
 
 };
